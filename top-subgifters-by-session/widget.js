787265c02fae4861cb3baa006dd9bb2f55f60f94
Rodrigo Caldas <75918726+c4ldas@users.noreply.github.com>
2023-07-23 01:25:51 +0100

    Adding subgifter manually by command

--- top-subgifters-by-session/widget.js
+++ top-subgifters-by-session/widget.js
@@ -6,8 +6,6 @@ window.addEventListener('onWidgetLoad', async (obj) => {
   mockData = fieldData.mockData.replaceAll("'",'"') // Replacing single quotes to double quotes for JSON support
   isEditor = obj.detail.overlay.isEditorMode
   
-  // console.log('onWidgetLoad: ', JSON.parse(mockData))
-  
   // When in Ovelray Editor, show mock data to test font, color, size, spacing, etc
   if(isEditor){
     sortTopGifters(JSON.parse(mockData))
@@ -17,6 +15,8 @@ window.addEventListener('onWidgetLoad', async (obj) => {
 
 
 window.addEventListener('onEventReceived', async (obj) => {
+  
+  console.log(obj)
   // When clicking on button "Test with mock data", it will show mock data on OBS to test
   if(obj.detail.event.listener == 'widget-button' && obj.detail.event.field == 'testWidget'){
     console.log('onEventReceived: ', JSON.parse(mockData))
@@ -24,12 +24,22 @@ window.addEventListener('onEventReceived', async (obj) => {
     return
   } 
   
+  // Manually add a subgifter (Only Streamer can do that)
+  if(obj.detail.listener == 'message' && obj.detail.event.data.nick == obj.detail.event.data.channel && obj.detail.event.data.text.startsWith(fieldData.command)){
+    
+    // Command example: !addgifter username value
+    const username = obj.detail.event.data.text.split(" ")[1]
+    const value = parseInt(obj.detail.event.data.text.split(" ")[2])
+    
+    gifterObj[username] ? gifterObj[username]+= value : gifterObj[username] = value
+    sortTopGifters(gifterObj)
+  }
+  
   // If not a subscription option, ignore
   if(obj.detail.listener != 'subscriber-latest') return
   
   // If subscription is a gift, start counting
-  if(obj.detail.event.gift) {    
-    // const sender = obj.detail.event.data.text || obj.detail.event.sender; // Remove this line
+  if(obj.detail.event.gifted) {    
     // Checking the sender (gifter)
     const sender = obj.detail.event.sender;
     // Adding one to the gifter key object. In case it is the first time gifting, create the gifter key with value 1
@@ -40,21 +50,23 @@ window.addEventListener('onEventReceived', async (obj) => {
 })
 
 
-// Sort the top gifters and show on screen
+// Sort the top gifters and show them on screen
 async function sortTopGifters(gifterObj){
   let array = []
 
   // Sorting gifters in descending order
   const keysSorted = Object.keys(gifterObj).sort(function(a,b){return gifterObj[b]-gifterObj[a]})
-
+  
   // Create the list with the amount chosen 
   for(let username in keysSorted){
+    // Only shows the amount requested    
     if(username >= fieldData.giftersToDisplay) break
     
+    // Select whether the amount of each subgifter will be shown
     fieldData.showAmount == "true" ? response = `${keysSorted[username]} - ${gifterObj[keysSorted[username]]}` : response = keysSorted[username]
     array.push(`<div class='top'> ${response} </div>`)
   }
-  // Show the gifters sorted on screen (HTML tab) and using the separator (if any)
+  // Show the gifters sorted on screen (HTML tab) and using the separator chosen (if any)
   document.querySelector("#container").innerHTML = array.toString().replaceAll(',', `${fieldData.separator}`)
   
 }
