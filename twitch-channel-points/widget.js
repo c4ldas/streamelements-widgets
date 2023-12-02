e6e23ec4ebefb4d3b164ed31d98cb5bd35d3bf05
Rodrigo Caldas <75918726+c4ldas@users.noreply.github.com>
2023-12-02 13:48:35 +0000

    Sending redemption to onEventReceived in the widget
    - Sends channel points redemption data to "onEventReceived" listener in the widget
    - obj.detail.listener is "channelPoints"
    - Sends basic data as well as raw data
    - Reloads widget in case it doesn't receive a PONG in the time specified by documentation or receive a RECONNECT request
    - Fixed PING sending timing 
    - Fixed PONG listening

--- twitch-channel-points/widget.js
+++ twitch-channel-points/widget.js
@@ -1,14 +1,20 @@
+var reloadOverlay
+const sendPingSeconds = 240000 // 4 minutes
+const reloadOverlayTime = 9000 // 9 seconds
+
+window.addEventListener('onEventReceived', async (obj) => {
+  if(obj.detail.listener == 'channelPoints') {
+    console.log(obj)
+  }
+})
 
 window.addEventListener('onWidgetLoad', async (obj) => {
-      
+
   const providerId = obj.detail.channel.providerId
-  const username = obj.detail.channel.username
-  
+  const username = obj.detail.channel.username  
   const socket = new WebSocket('wss://pubsub-edge.twitch.tv')
   const eventTopic = `community-points-channel-v1.${providerId}`
-  
-  // document.getElementById('text').innerText = `Twitch Channel Points - ${username}`
-  
+    
   // Connection and authentication  
   socket.onopen = (event) => {
     const info = {
@@ -17,23 +23,43 @@ window.addEventListener('onWidgetLoad', async (obj) => {
         "topics": [eventTopic]
       }
     }
-    
     console.log("Successfully connected to the websocket - Twitch!");
     socket.send(JSON.stringify(info));
     
-    setInterval( () => { sendPing() }, 120000)
+    // Send PING every 4 minutes to keep the connection alive
+    // https://dev.twitch.tv/docs/pubsub/#connection-management
+    setInterval( () => { sendPing() }, sendPingSeconds)
   }
   
+  // Send PING to keep the connection alive.
+  // Also wait 9 seconds to reload the widget in case there is no PONG
+  async function sendPing(){
+    socket.send(JSON.stringify( { type: "PING" } ));
+
+    // PING sent. If no PONG returned in 9 seconds, reload the widget to reauthenticate
+    // https://dev.twitch.tv/docs/pubsub/#connection-management
+    reloadOverlay = setTimeout( () => {
+      console.log("Pubsub reconnect!")
+      location.reload()      
+    }, reloadOverlayTime)
+  }  
  
   socket.onmessage = (event) => {
     const data = JSON.parse(event.data);
     
-    // If it is a PING, send a PONG to keep the connection alive
-    if(event.data.type == "PING"){      
-      sendPong();
+    // PONG received, cancel the timer to reload the widget
+    if(data.type == "PONG"){   
+      clearTimeout(reloadOverlay)
+      console.log("PONG received, overlay won't reload!")
     }
     
-     // In case it is a Channel Point Redemption, get the variables and call the function RedemptionRedeemed()
+    // RECONNECT status received, widget needs to be reloaded to reauthenticate
+    if(data.type == "RECONNECT"){
+      console.log("PubSub needs to reconnect, reloading it...")
+      location.reload()
+    }
+    
+    // In case it is a Channel Point Redemption, get the variables and call the function RedemptionRedeemed()
     if(data.data?.topic == eventTopic){
       const redemptionData = JSON.parse(data.data.message);
       
@@ -46,28 +72,30 @@ window.addEventListener('onWidgetLoad', async (obj) => {
       }
     }
   }
-
-  // Send PING to keep the connection alive
-  function sendPing(){
-    socket.send(JSON.stringify( { type: "PING" } ));
-  }
-   
-  // Send PONG to keep the connection alive
-  function sendPong(){
-    socket.send(JSON.stringify( { type: "PONG" } ));
-  }
   
   // Here you do whatever you want with the reward
-  function redemptionRedeemed(id, reward, user, user_input, image, background, redemptionData){    
-    // In the example below, it will show the redemption information on browser console and HTML during 20 seconds
-    console.table({ 'ID': id, 'Title': reward.title, 'Username': user.display_name, 'User Input': user_input, 'Image':  image, 'Background': background, 'Cost': reward.cost });
-    console.log(redemptionData.data);
+  function redemptionRedeemed(id, reward, user, user_input, image, background, redemptionData){ 
+    console.table({ 'id': id, 'title': reward.title, 'username': user.display_name, 'userInput': user_input, 'image':  image, 'background': background, 'cost': reward.cost });
+    
+    const data = { 
+      detail: { 
+        listener: "channelPoints", 
+        event: {
+          service: "twitch", 
+          data:  { 'id': id, 'title': reward.title, 'username': user.display_name, 'userInput': user_input, 'image':  image, 'background': background, 'cost': reward.cost },
+          rawData: redemptionData.data,
+        }
+      }
+    }
+    
+    const channelPointsEvent = new CustomEvent("onEventReceived", data)
+    window.dispatchEvent(channelPointsEvent);
 
-    document.getElementById('redemption').style.background = `no-repeat center/75% url("${image}"), ${background}`;
-    document.getElementById('redemptionMessage').innerText = user_input ? user_input : ''
-    setTimeout( () => {
-      document.getElementById('redemption').style.background = ''
-      document.getElementById('redemptionMessage').innerText = ''
-    }, 20000);
+    // document.getElementById('redemption').style.background = `no-repeat center/75% url("${image}"), ${background}`;
+    // document.getElementById('redemptionMessage').innerText = user_input ? user_input : ''
+    // setTimeout( () => {
+    //   document.getElementById('redemption').style.background = ''
+    //   document.getElementById('redemptionMessage').innerText = ''
+    // }, 20000);
   }
 })
